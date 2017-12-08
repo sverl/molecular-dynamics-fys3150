dea7807bdd1569d3fe48af2cc431f02dbae9778d
Unknown <teamsloslo@gmail.com>
2017-12-08 19:22:43 +0100

    Refactored to shorter variable names

--- main.cpp
+++ main.cpp
@@ -12,24 +12,22 @@
 using namespace std;
 
 int main(int numberOfArguments, char** argumentList) {
-  int numberOfUnitCells = 5;
-  double initialTemperature =
-      UnitConverter::temperatureFromSI(300.0);  // measured in Kelvin
-  double latticeConstant =
-      UnitConverter::lengthFromAngstroms(5.26);  // measured in angstroms
+  int n_cells = 5;
+  double temp_init =
+      UnitConverter::temperatureFromSI(300.0);          // measured in Kelvin
+  double b = UnitConverter::lengthFromAngstroms(5.26);  // measured in angstroms
 
   // If a first argument is provided, it is the number of unit cells
   if (numberOfArguments > 1)
-    numberOfUnitCells = atoi(argumentList[1]);
+    n_cells = atoi(argumentList[1]);
   // If a second argument is provided, it is the initial temperature (measured
   // in kelvin)
   if (numberOfArguments > 2)
-    initialTemperature =
-        UnitConverter::temperatureFromSI(atof(argumentList[2]));
+    temp_init = UnitConverter::temperatureFromSI(atof(argumentList[2]));
   // If a third argument is provided, it is the lattice constant determining the
   // density (measured in angstroms)
   if (numberOfArguments > 3)
-    latticeConstant = UnitConverter::lengthFromAngstroms(atof(argumentList[3]));
+    b = UnitConverter::lengthFromAngstroms(atof(argumentList[3]));
 
   double dt = UnitConverter::timeFromSI(1e-15);  // Measured in seconds.
 
@@ -45,29 +43,30 @@ int main(int numberOfArguments, char** argumentList) {
        << " K" << endl;
 
   System system;
-  system.createFCCLattice(numberOfUnitCells, latticeConstant,
-                          initialTemperature);
   system.potential().setEpsilon(1.0);
   system.potential().setSigma(1.0);
+    system.createFCCLattice(n_cells, b, temp_init);
 
   system.removeTotalMomentum();
 
   StatisticsSampler statisticsSampler;
   IO movie("movie.xyz");  // To write the state to file
 
-  cout << setw(20) << "Timestep" << setw(20) << "Time" << setw(20)
-       << "Temperature" << setw(20) << "KineticEnergy" << setw(20)
-       << "PotentialEnergy" << setw(20) << "TotalEnergy" << endl;
-  for (int timestep = 0; timestep < 1000; timestep++) {
+  cout << setw(16) << "Timestep" << setw(16) << "Time" << setw(16)
+       << "Temperature" << setw(16) << "KineticEnergy" << setw(16)
+       << "PotentialEnergy" << setw(16) << "TotalEnergy" << endl;
+  for (int timestep = 0; timestep < 10000; timestep++) {
     system.step(dt);
     statisticsSampler.sample(system);
     if (timestep % 100 == 0) {
       // Print the timestep every 100 timesteps
-      cout << setw(20) << system.steps() << setw(20) << system.time()
-           << setw(20) << statisticsSampler.temperature() << setw(20)
-           << statisticsSampler.kineticEnergy() << setw(20)
-           << statisticsSampler.potentialEnergy() << setw(20)
+      cout << setw(16) << system.steps() << setw(16) << system.time()
+           << setw(16) << statisticsSampler.temperature() << setw(16)
+           << statisticsSampler.kineticEnergy() << setw(16)
+           << statisticsSampler.potentialEnergy() << setw(16)
            << statisticsSampler.totalEnergy() << endl;
+
+
     }
     movie.saveState(system);
   }
